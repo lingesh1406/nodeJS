f437fcd92789888745a2a7180ee491e2ab498d72
lingesh1406 <lingeshwaran3496@gmail.com>
2023-09-27 22:39:53 +0530

    node training

---
+++ sep27.js
@@ -0,0 +1,24 @@
+
+//     Reading files(text) 
+
+// const fs=require('fs');                              => it will importing the file module
+// const read=fs.readFileSync("./sep27.txt",'Utf-8');   =>readFileSync is a predifined funcition in that modeule 
+// console.log(read);                                     ||> using this it will run the txt file ('Utf-8 => is must')
+
+
+
+//      Writing (text) files
+
+// const fs=require('fs');                   =>it will import the  module    
+// const write = fs.writeFileSync('./write.txt',"hey lingesh welcome to nodeJS");  ==>(writeFileSync)using this it will create the txt file   
+// console.log(write);                                  ||> this will add to the text file
+
+
+
+
+
+
+// const fs=require('fs');                            ==> used to import the file module 
+// const read=fs.readFileSync('./write.txt','Utf-8'); ==>using this read the created txt file 
+// console.log(read);                           ||>(it is must!)
+//                                  
